680defb178a8c4a6d509797792c8b1e313ff1e06
JOSE ALBERTO <josealberto12345@hotmail.es>
2018-06-30 03:49:55 +0200

    uso eficiente del FOR .

---
+++ FOR_recorrer_lista_eficientemente.py
@@ -0,0 +1,7 @@
+
+mi_lista = [34, 21, 187, 73, 12, 43]
+
+for item in mi_lista:                                                                                                   #Esto funciona como todo lo anterior, por cada uno de los items que estan dentro de mi lista, ejecuta este código#
+    print("Tengo que comprar {}".format(item))
+
+print("Esta es la lista de la compra")
