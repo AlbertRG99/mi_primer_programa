ea470539ebc9d50c79daa5caf3c5684aa726b10f
JOSEA <josealberto12345@hotmail.es>
2018-07-03 01:49:02 +0200

    calculador de medias sin acabar

--- calculador_media.py
+++ calculador_media.py
@@ -1,17 +1,19 @@
 """
 Crea un programa que calcule la media de los elementos de la lista de números introducida por el usuario (media = suma de todos los numeros / numero de numeros )
 """
+# Programa para demostrar (MEDIA ARITMÉTICA) mean()
+# Función para el módulo STATICS
 
+# Importar módulo STATICS
+import statistics
+
+# Lista de numeros enteros positivos
 lista_usuario = []
-contador_elementos = 0
 
-while lista_usuario != "FIN":
-    print("Introduce los núnmeros y escribe 'FIN' para continuar: ")
-    contador_elementos += 1
-    numero_inicial = lista_usuario[0]
-    for elemento in lista_usuario:
+while lista_usuario != "00"
+    lista_usuario = input("Introduce los números: (Escribe '00' para finalizar)")
 
+x = statistics.mean(lista_usuario)
 
-
-
-lista_usuario.remove("FIN")
+# Imprimiendo la media
+print("Mean is :", x)
