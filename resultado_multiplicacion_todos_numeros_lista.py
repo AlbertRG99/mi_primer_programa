61ea1d5d1057b86e06631c5494c5d23c8fe84cf7
JOSE ALBERTO <josealberto12345@hotmail.es>
2018-07-04 02:29:23 +0200

    resultado de la multiplicación de todos los numeros de una lista

---
+++ resultado_multiplicacion_todos_numeros_lista.py
@@ -0,0 +1,7 @@
+"""
+Dada una lista de números, devolver el resultado de la multiplicación de todos los números.
+"""
+
+lista_numeros = [1, 4, 5, 76, 8564, 24]
+
+print(lista_numeros[0]*lista_numeros[1]*lista_numeros[2]*lista_numeros[3]*lista_numeros[4]*lista_numeros[5])
