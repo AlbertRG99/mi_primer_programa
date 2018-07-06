f16da0538158b1c879307972427dd546c4bf6101
JOSE ALBERTO <josealberto12345@hotmail.es>
2018-07-06 02:36:47 +0200

    sustitución valores string por su numero correspondiente

---
+++ sustitucion_valor_A.py
@@ -0,0 +1,3 @@
+"""
+Crear un programa que cambie todas las ‘A’ o ‘a’ por la string ‘VACA’ de una string introducida por el usuario.
+"""
---
+++ sustitucion_vocales_por_su_numero_de_aparicion.py
@@ -0,0 +1,14 @@
+"""
+Crea un programa que cambie las vocales por su numero de aparición. Por ejemplo: La string “aurora boreal” se convertiría en “12r3r4 b5r67l”
+"""
+
+frase_usuario = input("Introduce una frase y cambiaré sus vocales por el numero de aparición: ")
+vocales = ["a", "e", "i", "o", "u"]
+contador = 0
+
+for letra in frase_usuario:
+    if letra in vocales:
+        contador += 1
+        frase_usuario = frase_usuario.replace(letra, str(contador))
+
+print(frase_usuario)
