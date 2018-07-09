2ea83f5577b0267a80714259f3e08435c5d4e479
JOSEA <josealberto12345@hotmail.es>
2018-07-10 01:19:58 +0200

    mi primera función

---
+++ funcion_numero_mas_grande_entre_tres.py
@@ -0,0 +1,5 @@
+def numero_mas_grande_entre_tres (num1, num2, num3):
+    numero_mas_grande = max(num1, num2, num3)
+    return numero_mas_grande
+
+maximo = print(numero_mas_grande_entre_tres(1, 5, 456))
